0c068ff884e6f2c9db47b330556a05a0936e1fa3
Juanma Carmona <124081936+Carmona97@users.noreply.github.com>
2023-11-06 20:49:34 +0100

    Añado metodo insertarEnDepartamento

--- Unidad3/proyectoEmpDep/src/main/java/org/example/Departamento.java
+++ Unidad3/proyectoEmpDep/src/main/java/org/example/Departamento.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Departamento {
 
     private int id;
@@ -55,4 +60,36 @@ public class Departamento {
     public void setEmpleados(ArrayList<Empleado> empleados) {
         this.empleados = empleados;
     }
+
+    public boolean insertarEnDepartamento(Connection conn, String nombre, double presupuesto, double gastos) {
+        String query = "INSERT INTO departamento (nombre,presupuesto,gastos) VALUES (?,?,?)";
+
+        boolean insercionCorrecta =false;
+        if (conn != null) {
+
+            try (PreparedStatement consulta = conn.prepareStatement(query)) {
+
+                consulta.setString(1, nombre);
+                consulta.setDouble(2, presupuesto);
+                consulta.setDouble(3, gastos);
+
+                int registrosAfectados = consulta.executeUpdate();
+                if (registrosAfectados != 0) {
+
+                    insercionCorrecta = true;
+
+                } else {
+
+                    insercionCorrecta = false;
+
+                }
+
+            } catch (SQLException ex) {
+                System.err.printf("Se ha producido un error al ejecutar la consulta SQL.");
+            }
+        }
+        return insercionCorrecta;
+    }
+
 }
+
