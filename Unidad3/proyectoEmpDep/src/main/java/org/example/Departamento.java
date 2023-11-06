edc26dc88773d74180a97352bd7c6664cf1d098b
Juanma Carmona <124081936+Carmona97@users.noreply.github.com>
2023-11-06 20:03:23 +0100

    Creacion de clases

--- Unidad3/proyectoEmpDep/src/main/java/org/example/Departamento.java
+++ Unidad3/proyectoEmpDep/src/main/java/org/example/Departamento.java
@@ -1,4 +1,58 @@
 package org.example;
 
 public class Departamento {
+
+    private int id;
+    private String nombre;
+    private double presupuesto;
+    private double gastos;
+    private ArrayList<Empleado> empleados;
+
+    public Departamento(int id, String nombre, double presupuesto, double gastos, ArrayList<Empleado> empleados) {
+        this.id = id;
+        this.nombre = nombre;
+        this.presupuesto = presupuesto;
+        this.gastos = gastos;
+        this.empleados = empleados;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(double presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+    public double getGastos() {
+        return gastos;
+    }
+
+    public void setGastos(double gastos) {
+        this.gastos = gastos;
+    }
+
+    public ArrayList<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(ArrayList<Empleado> empleados) {
+        this.empleados = empleados;
+    }
 }
--- Unidad3/proyectoEmpDep/src/main/java/org/example/Empleado.java
+++ Unidad3/proyectoEmpDep/src/main/java/org/example/Empleado.java
@@ -1,4 +1,67 @@
 package org.example;
 
 public class Empleado {
+    private int id;
+    private String dni;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private Departamento idDepartamento;
+
+    public Empleado(int id, String dni, String nombre, String apellido1, String apellido2, Departamento idDepartamento) {
+        this.id = id;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.idDepartamento = idDepartamento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public Departamento getIdDepartamento() {
+        return idDepartamento;
+    }
+
+    public void setIdDepartamento(Departamento idDepartamento) {
+        this.idDepartamento = idDepartamento;
+    }
 }
