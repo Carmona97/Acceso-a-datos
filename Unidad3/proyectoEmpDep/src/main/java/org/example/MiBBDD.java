c8b4286e5dd611b32950aabac36ebe22f5abce10
Juanma Carmona <juanmacarmona97@gmail.com>
2023-11-10 14:31:14 +0100

    Creacion de las clases del proyecto

--- Unidad3/proyectoEmpDep/src/main/java/org/example/MiBBDD.java
+++ Unidad3/proyectoEmpDep/src/main/java/org/example/MiBBDD.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class MiBBDD {
@@ -39,5 +40,26 @@ public class MiBBDD {
             e.printStackTrace();
         }
         return conexionCerrada;
+    }
+
+    public void crearTablaEmpleado(){
+
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement("CREATE TABLE IF NOT EXISTS empleado(id SERIAL PRIMARY KEY, nif VARCHAR(9) NOT NULL UNIQUE,nombre VARCHAR(100) NOT NULL,apellido1 VARCHAR(100) NOT NULL,apellido2 VARCHAR(100),id_departamento INT, FOREIGN KEY (id_departamento) REFERENCES departamento(id);)");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            ps.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            ps.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
