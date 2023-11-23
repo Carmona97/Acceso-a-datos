be20eaabb2869834f0fa1e69d149c9f4878feb40
Juanma Carmona <124081936+Carmona97@users.noreply.github.com>
2023-11-23 20:26:00 +0100

    ejercicio 3 terminado

--- ExamenUnidad2/eje3_JuanManuel_CarmonaRuiz/src/ControladorEventos.java
+++ ExamenUnidad2/eje3_JuanManuel_CarmonaRuiz/src/ControladorEventos.java
@@ -2,34 +2,49 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-public class ControladorEventos  extends DefaultHandler {
+public class ControladorEventos extends DefaultHandler {
 
     protected String contenido;
+    protected String titulo;
+    protected String semilla;
+    protected String palabrasClave;
+    protected String estado;
 
+    protected boolean activo;
 
-    public void startElement(String uri, String nombre,String apodo,String puesto, int dorsal, String descripcion, String qName, Attributes atributos) throws SAXException {
-
-        if (qName.equals("jugador")) {
-
-            System.out.println(atributos.getValue("nombre")+" "+atributos.getValue("apodo")+" "+atributos.getValue("puesto")+" "+atributos.getValue("dorsal")+" "+atributos.getValue("descripcion"));
-        }
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 
     }
 
 
-    public void characters(char[] ch, int start, int length) throws SAXException{
+    public void characters(char[] ch, int start, int length) throws SAXException {
         contenido = new String(ch, start, length);
 
     }
 
 
-    public void endElement(String uri, String localName, String qName) throws SAXException{
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
 
-        if(!qName.isBlank()){
-            if(qName.equals("jugador")){
-                System.out.println(" "+qName+": "+contenido);
+        if (qName.equals("Título") && !contenido.equals("null")) {
+            titulo="Titulo " + contenido;
+        } else if (qName.equals("Semilla") && !contenido.equals("null")) {
+            semilla+="Semilla " + contenido ;
+        } else if (qName.equals("Palabras_clave") && !contenido.equals("null")) {
+            palabrasClave+="Palabras " + contenido ;
+        } else if (qName.equals("Estado") && !contenido.equals("null")) {
+            activo = false;
+            if(contenido.equals("Activa")){
+                estado+="Estado "+contenido;
+                activo =true;
+            }
+        } else if (qName.equals("item") && !contenido.equals("null")) {
+            if(activo = true){
+                System.out.println(titulo+" "+semilla+" "+palabrasClave+" "+estado);
             }
         }
+
     }
 
 }
