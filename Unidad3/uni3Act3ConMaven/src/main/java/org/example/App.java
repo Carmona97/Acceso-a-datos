a667aa9318cedcadb50aa14af41a638b34d359d7
Juanma Carmona <124081936+Carmona97@users.noreply.github.com>
2023-11-25 20:02:25 +0100

    Update App.java

--- Unidad3/uni3Act3ConMaven/src/main/java/org/example/App.java
+++ Unidad3/uni3Act3ConMaven/src/main/java/org/example/App.java
@@ -2,10 +2,6 @@ package org.example;
 
 import java.sql.*;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
@@ -13,25 +9,24 @@ public class App {
         String user = "postgres";
         String pass = "123";
         Connection conn = DriverManager.getConnection(url, user, pass);
+
         PreparedStatement ps = null;
-        String anadidoCorrectamente;
-        try {
-            ps = conn.prepareStatement("INSERT INTO asignatura(nombre,anyo) VALUES (?,?)");
-            ps.setString(1, "Lenguaje de marcas");
-            ps.setInt(2, 1);
-            int registrosAfectados = ps.executeUpdate();
-            if (registrosAfectados !=0) {
-                System.out.println("Se ha añadido el registro correctamente ");
-            }
-        } catch (SQLException e) {
-            System.out.println("Ha habido algun error al registrar la asignatura");
+
+        ps = conn.prepareStatement("INSERT INTO asignatura(nombre,anyo) VALUES (?,?)");
+        ps.setString(1, "Lenguaje de marcas");
+        ps.setInt(2, 1);
+        int registrosAfectados = ps.executeUpdate();
+        if (registrosAfectados != 0) {
+            System.out.println("Se ha añadido el registro correctamente ");
+        } else {
+            System.out.println("No se ha podido añadir correctamente el registro");
         }
         Statement statement = conn.createStatement();
         String SQLsentence = "SELECT * FROM asignatura ORDER BY codigo";
         ResultSet rs = statement.executeQuery(SQLsentence);
         System.out.println("Codigo" + "\t" + "Nombre" + "\t" + "Anyo");
         while (rs.next()) {
-            System.out.println(rs.getString(1) + "\t" + rs.getString(2) +"\t" + rs.getString(3));
+            System.out.println(rs.getString(1) + "\t" + rs.getString(2) + "\t" + rs.getString(3));
         }
         rs.close();
         conn.close();
