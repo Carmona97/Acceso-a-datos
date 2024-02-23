43ce2ce600ca043e3b604c99c7d413e5f0823956
Juanma Carmona <juanmacarmona97@gmail.com>
2024-02-23 12:28:49 +0100

    4.7 y 4.8

---
+++ Unidad4/Act4.9/src/main/java/entidades/Cuenta.java
@@ -0,0 +1,49 @@
+package entidades;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Inheritance(strategy= InheritanceType.TABLE_PER_CLASS)
+public class Cuenta implements Serializable {
+
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String titular;
+    private double balance;
+    private double tipoInteres;
+
+    public Cuenta() {
+        super();
+    }
+    public long getId() {
+        return this.id;
+    }
+
+
+    public String getTitular() {
+        return this.titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public double getTipoInteres() {
+        return this.tipoInteres;
+    }
+
+    public void setTipoInteres(double tipoInteres) {
+        this.tipoInteres = tipoInteres;
+    }
+
+}
